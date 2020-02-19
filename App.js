d25c2e73a782b50d638744ca3d107199836dcb61
juuze <jass2@ic.ufal.br>
2020-02-18 21:55:04 -0300

    Login screen built. Still need logic and other screns.

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state={
+    email:"",
+    password:""
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>RuApp</Text>
+        <Text style={styles.sublogo}>Veja o cardápio e agende sua refeição !</Text>
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Digite sua matrícula" 
+            placeholderTextColor="#000000"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Digite sua senha" 
+            placeholderTextColor="#000000"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Esqueceu a senha?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>ENTRAR</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.signupText}>cadastro</Text>
+        </TouchableOpacity>
+
+  
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +49,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#FF0000",
+    marginBottom:10
+  },
+  sublogo:{
+    fontWeight:"italic",
+    fontSize:15,
+    color:"#000000",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#ff8080",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"#000000"
+  },
+  forgot:{
+    color:"#000000",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#ff1a1a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"#FFF"
+  },
+  signupText:{
+    collor:"#000000"
+  }
 });
